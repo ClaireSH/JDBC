df5651ad383d7e473c7bf3b7c42de065920ed906
최서현 <chltjguss@naver.com>
2016-05-20 12:21:27 +0900

    DB 0520 update

--- src/brokerJdbc/dao/Database.java
+++ src/brokerJdbc/dao/Database.java
@@ -5,10 +5,13 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import brokerJdbc.Exception.DuplicateIDException;
 import brokerJdbc.Exception.RecordNotFoundException;
 import brokerJdbc.vo.Customer;
+import brokerJdbc.vo.Shares;
+import brokerJdbc.vo.Stock;
 
 //업무로직을 담는 메소드
 public class Database {
@@ -158,7 +161,8 @@ public class Database {
 		
 	}
 	/**
-	 * 매개변수로 주어진 ssn에 해당하는 고객 정보를 삭제 
+	 * 매개변수로 주어진 ssn에 해당하는 고객 정보 및 보유주식 정보를 삭제 
+	 * 트랜잭션 처리 해줘야함. 
 	 * @param ssn
 	 * @throws RecordNotFoundException
 	 */
@@ -166,25 +170,165 @@ public class Database {
 		if(!ssnExist(ssn)) throw new RecordNotFoundException();
 		Connection con = ConnectionManager.getConnection();
 		String sql = "DELETE FROM customer WHERE ssn = ?";
+		String sql1 = "DELETE FROM shares WHERE ssn = ?";
 		try{
+		con.setAutoCommit(false);
 		PreparedStatement pstmt = con.prepareStatement(sql);
 		pstmt.setString(1, ssn);
-		int row = pstmt.executeUpdate();
-		System.out.println(row+"개 레코드 삭제 됨.");
+		pstmt.executeUpdate();
+		if(true) throw new SQLException();
+		PreparedStatement pstmt1 = con.prepareStatement(sql1);
+		pstmt1.setString(1, ssn);
+		//int row = pstmt.executeUpdate();
+		int row1 = pstmt1.executeUpdate();
+		//System.out.println(row+"개 레코드와 그 객체의 보유주식 "+row1+"개 삭제 됨.");
+		con.commit();
 		}catch(SQLException e){
 			e.printStackTrace();
 		}finally{
 			ConnectionManager.close(con);
 		}
 	}
+	/**어떤 객체를 매번 새로 생성하고 버리기보다 효율적으로 관리하기 위해
+	 *  Connection 계속 만들고 버리는 것은 소모적인 작업. 
+	 *  Object pool: 일정수의 객체를 미리  생성해 모아 놓고 빌려줌. 버리지 않음. 
+	 * 	
+	 */
+	/**
+	 * 매개변수로 주어진 새로운 고객정보를 갱신한다. 
+	 * @param c 새로운 고객 정보를 가지고 있는 Customer 객체
+	 * @throws RecordNotFoundException
+	 */
+	
+	public void updateCustomer(Customer c)throws RecordNotFoundException{
+	 if(!ssnExist(c.getSsn())) throw new RecordNotFoundException();
+	 Connection con = ConnectionManager.getConnection();
+	 String sql = "UPDATE customer SET cust_name = ?, address =? WHERE ssn = ?";
+	 try{
+	 PreparedStatement pstmt = con.prepareStatement(sql);
+	 pstmt.setString(1, c.getCust_name());
+	 pstmt.setString(2, c.getAddress());
+	 pstmt.setString(3, c.getSsn());
+	 int row = pstmt.executeUpdate();
+	 System.out.println(row );
+	 
+	 }catch(SQLException e){
+		 e.printStackTrace();
+	 }finally{
+		 ConnectionManager.close(con);
+	 }
+	}
+	/**
+ 	 * 매개변수로 주어진 ssn의 고객이 보유하고 있는 주식의 목록을 조회하여 반환
+	 * @param ssn
+	 * @return
+	 * @throws RecordNotFoundException
+	 */
+	
+	public ArrayList<Shares> getPortfolio(String ssn)throws RecordNotFoundException{
+		//특정 고객이 보유하고 있는 모든 주식의 정보 
+		if(!ssnExist(ssn)) throw new RecordNotFoundException();
+		ArrayList<Shares> list = new ArrayList<>();
+		Connection con = ConnectionManager.getConnection();
+		String sql ="SELECT * FROM shares WHERE ssn= ?";
+		try{
+		PreparedStatement pstmt = con.prepareStatement(sql);
+		pstmt.setString(1, ssn);
+		ResultSet rs = pstmt.executeQuery();
+		Shares sh = null;
+			while (rs.next()) {
+			String symbol = rs.getString("symbol");
+			String quantity = rs.getString("quantity");
+			 sh = new Shares(ssn, symbol, quantity);
+			list.add(sh);
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			ConnectionManager.close(con);
+		}
+		
+		
+		return list;
+	}
+	
+	/**
+	 * Customer 테이블에 등록된 모든 고객정보를 조회한다. 
+	 * @return 등록되어 있는 모든 고객정보 목록
+	 */
+	
+	public ArrayList<Customer> getAllCustomers(){
+		ArrayList<Customer> list = new ArrayList<>();
+		Connection con = ConnectionManager.getConnection();
+		String sql = "SELECT * FROM customer";
+		try{
+		PreparedStatement stmt = con.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		while(rs.next()){
+			String ssn = rs.getString("ssn");
+			String cust_name = rs.getString("cust_name");
+			String address = rs.getString("address");
+		Customer c = new Customer(ssn, cust_name, address);
+		list.add(c);
+			
+		}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			ConnectionManager.close(con);
+		}
+		
+		return list;
+	}
+	
+	public ArrayList<Stock> getAllStocks(){
+		ArrayList<Stock> list = new ArrayList<>();
+		Connection con = ConnectionManager.getConnection();
+		String sql = "SELECT * FROM Stock";
+		try{
+		PreparedStatement stmt = con.prepareStatement(sql);
+		ResultSet rs = stmt.executeQuery();
+		while(rs.next()){
+			String symbol = rs.getString("symbol");
+			String price = rs.getString("price");
+		Stock st = new Stock(symbol, price );
+		list.add(st);
+			
+		}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			ConnectionManager.close(con);
+		}
+		
+		
+		return list;
+		
+	}
+	
 	
 public static void main(String[] args) throws SQLException, RecordNotFoundException, DuplicateIDException {
 	Database db = new Database();
 //	System.out.println(db.ssnExist("111-111"));
 //	System.out.println(db.getCustomer("111-111"));
-//	Customer c = new Customer("111-120","최서현","서울");
+//	Customer c = new Customer("111-120","김진홍","여의도");
 //	db.addCustomer(c);
-	db.deleteCustomer("111-120");
+//	db.deleteCustomer("111-120");
+	
+//	db.updateCustomer(c);
+//	ArrayList<Shares> list =  db.getPortfolio("111-112");
+//	for(Shares sh : list){
+//		System.out.println(sh);
+//	}
+//	ArrayList<Customer> culist = db.getAllCustomers();
+//	for(Customer c : culist){
+//		System.out.println(c.toString());
+//	}
+	ArrayList<Stock> stlist = db.getAllStocks();
+	for(Stock st : stlist){
+		System.out.println(st.toString());
+	}
+	
 }
 }
 
